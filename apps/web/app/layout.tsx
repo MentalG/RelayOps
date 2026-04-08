6e388f974ff674dd79ce1fb75237ab0402532017
Illia Hromov <gromovthefirst@gmail.com>
2026-04-08 23:10:25 +0300

    init: web

---
+++ apps/web/app/layout.tsx
@@ -0,0 +1,18 @@
+import { Inter } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const inter = Inter({subsets:['latin'],variable:'--font-sans'});
+
+
+// app/layout.tsx
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en" className={cn("font-sans", inter.variable)}>
+      <body>{children}</body>
+    </html>
+  )
+}
